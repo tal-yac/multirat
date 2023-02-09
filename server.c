360b021c5f9fd71291851b89faf51a0d00550e64
Tal <talycb8@gmail.com>
2023-02-09 19:55:55 +0200

    Remove moved functions

--- server.c
+++ server.c
@@ -1,7 +1,6 @@
 #include "server.h"
 
 #include <stdio.h>
-#include <ws2tcpip.h>
 
 
 int server() {
@@ -86,105 +85,4 @@ int server() {
   }
   closesocket(client);
   return 0;
-}
-
-void parseArgv(int c, char **v, LaunchOption *lo) {
-  if (c == 1) {
-    printf("Choose an option:\r\n\t1. Server\r\n\t2. Client\r\n\t0. QUIT\r\n");
-    *lo = getlaunchoption();
-    return;
-  } else if (c > 2)
-    return;
-  if (*v[1]++ != '-') {
-    *lo = UNKNOWN_LAUNCH_OPTION;
-    return;
-  }
-  switch (*v[1]) {
-  case 's':
-    *lo = SERVER;
-    break;
-  case 'c':
-    *lo = CLIENT;
-    break;
-  default:
-    *lo = UNKNOWN_LAUNCH_OPTION;
-    return;
-  }
-}
-
-LaunchOption getlaunchoption() { return getchar() - '0'; }
-
-int client() {
-  AddrInfo *result, *ptr, hints;
-  result = ptr = NULL;
-  setaddrinfo(&hints, CLIENT);
-  if (getaddrinfo(LOCAL_HOST, DEFAULT_PORT, &hints, &result) != 0) {
-    printf("getaddrinfo failed, error: %d\n", WSAGetLastError());
-    return 1;
-  }
-  SOCKET conn = INVALID_SOCKET;
-  if (ptr == result)
-    printf("equal\n");
-  else
-    ptr = result;
-  conn = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-  if (conn == INVALID_SOCKET) {
-    printf("socket creating failed, error: %d\n", WSAGetLastError());
-    freeaddrinfo(result);
-    return 1;
-  }
-  if (connect(conn, result->ai_addr, (int)ptr->ai_addrlen) == SOCKET_ERROR) {
-    closesocket(conn);
-    conn = INVALID_SOCKET;
-  }
-  freeaddrinfo(result);
-  if (conn == INVALID_SOCKET) {
-    printf("error: unable to connect to the server\n");
-    return 1;
-  }
-  char msg[] = "hello this is client\r\n";
-  char buf[DEFAULT_BUFLEN];
-  if (send(conn, msg, (int)strlen(msg), 0) == SOCKET_ERROR) {
-    printf("send failed, error: %d\n", WSAGetLastError());
-    closesocket(conn);
-    return 1;
-  }
-  printf("sent");
-  int recbytes;
-  do {
-    recbytes = recv(conn, buf, DEFAULT_BUFLEN, 0);
-    if (recbytes > 0) {
-      printf("%s\n", buf);
-      break;
-    } else if (recbytes == 0) {
-      printf("connection closed");
-    } else {
-      printf("recv failed, error: %d\n", WSAGetLastError());
-    }
-  } while (recbytes > 0);
-  if (shutdown(conn, SD_SEND) == SOCKET_ERROR) {
-    printf("shutdown failed, error: %d", WSAGetLastError());
-    closesocket(conn);
-    return 1;
-  }
-  closesocket(conn);
-  return 0;
-}
-
-void setaddrinfo(AddrInfo *ai, LaunchOption lo) {
-  ZeroMemory(ai, sizeof(*ai));
-  (lo == SERVER) ? setaddrinfoserver(ai) : setaddrinfoclient(ai);
-}
-
-void setaddrinfoserver(AddrInfo *ai) {
-  ai->ai_family = AF_INET;
-  ai->ai_socktype = SOCK_STREAM;
-  ai->ai_protocol = IPPROTO_TCP;
-  ai->ai_flags = AI_PASSIVE;
-}
-
-void setaddrinfoclient(AddrInfo *ai) {
-  ai->ai_family = AF_INET;
-  ai->ai_socktype = SOCK_STREAM;
-  ai->ai_protocol = IPPROTO_TCP;
 }
