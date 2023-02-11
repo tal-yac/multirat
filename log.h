e33ee12f0ada10100f36dae8419ddae2fcb10f63
Tal <talycb8@gmail.com>
2023-02-11 22:38:57 +0200

    Add log levels

--- log.h
+++ log.h
@@ -7,10 +7,16 @@
 #define LOG_INFO 0
 #define LOG_DEBUG 1
 
-#if LOG_LEVEL >= LOG_DEBUG
-#define LOG(s, f, ...)                                                         \
-  fprintf(LOG_FILE, "%s::%s %d: " f "\n", __FILE__, __FUNCTION__, __LINE__,    \
+#define LOG(f, ...)                                                            \
+  fprintf(LOG_FILE, "%s::%s %d " f "\n", __FILE__, __FUNCTION__, __LINE__,    \
           ##__VA_ARGS__)
+
+#if LOG_LEVEL >= LOG_DEBUG
+#undef LOG_DEBUG
+#define LOG_DEBUG(f, ...) LOG(f, ##__VA_ARGS__)
 #else
-#define LOG(s, f, ...)
+#undef LOG_DEBUG
+#define LOG_DEBUG(f, ...)
 #endif // LOG_LEVEL >= LOG_DEBUG
+
+#define LOG_ERR(f, ...) LOG("error: " f, ##__VA_ARGS__)
